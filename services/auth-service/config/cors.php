13e7b7b06d365c303b0f232b7aa246688f3caa54
ali-rostom1 <ali.rostom220@gmail.com>
2025-06-28 17:53:11 +0100

    fix frontend login

--- services/auth-service/config/cors.php
+++ services/auth-service/config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Your frontend URL
+    'allowed_origins' => ['http://localhost:5173','http://localhost:4173'], // Your frontend URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
